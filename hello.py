05a03916c39ac300f04efd92766c8a248cf43dd6
max467919556 <38893119+max467919556@users.noreply.github.com>
2018-05-08 23:07:59 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,17 +1,9 @@
-def  application  ( environ ,  start_response ):
-
-    QUERY_STRING = '/?a=1&a=2&b=3'
-
-    response_body = [(i + '\n') for i in environ['QUERY_STRING'].split('&')]
-	
-   
-    status  =  '200 OK'
-
-    # Теперь тип содержимого - text / html 
-    response_headers  =  [ 
-        ( 'Content-Type' ,  'text / html' ), 
-        ( 'Content-Length' ,  str ( len ( response_body ))) 
-    ]
-
-    start_response ( status ,  response_headers ) 
-    return  [response_body]
+def app(environ, start_response):
+    status = "200 OK"
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+    body = ""
+    query_list = environ["QUERY_STRING"].split("&")
+    for item in query_list:
+        body += "{}\n".format(item)
+    return [ body.encode() ]
